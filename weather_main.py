3bbc28f7065db076750b75e744d269ba0da398df
GoDoVoReZ <univerhochu3005@gmail.com>
2021-04-27 14:52:39 +0500

    browser version added

--- weather_main.py
+++ weather_main.py
@@ -1,2 +1,24 @@
 import streamlit as st 
 from api_script import get_weather
+import time
+
+st.title('Weather in your city')
+text = st.text_input('Enter city here')
+btn = st.button('Find')
+
+plot_check = st.sidebar.checkbox('Show the plot?')
+time_check = st.sidebar.checkbox('Show the time?')
+
+if text is not None and btn:
+    try:
+        data = get_weather(text)
+        if time_check:
+            mess = 'Weather in {} ({}): {} C, {}'.format(data['City'], data['Time'], data['Temp'], data['Prec'])
+            st.markdown(f'# {mess}')
+        else:
+            mess = 'Weather in {}: {} C, {}'.format(data['City'], data['Temp'], data['Prec'])
+            st.markdown(f'# {mess}')
+        if plot_check:
+            pass
+    except:
+        st.write('Something went wrong.Try again')
